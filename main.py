6d4ffe6020224b5ab9de7b3c38ade8bbeae0483f
Uliana Degtiareva <uvdegtiareva@edu.hse.ru>
2024-11-19 00:02:32 +0500

    исправили функцию

--- main.py
+++ main.py
@@ -26,11 +26,12 @@ def fibonacci_iter(n: int) -> int:
         return None
     if n == 1 or n ==2:
         return 1
-    arr = [0]*(n+1)
-    arr[1] = arr[2] = 1
-    for i in range (3, n+1):
-        arr[i] = arr[i-1]+arr[i-2]
-    return arr[n]
+
+    arr = [0] * n
+    arr[0], arr[1] = 1, 1
+    for idx in range(2, n):
+        arr[idx] = arr[idx - 1] + arr[idx - 2]
+    return arr[n - 1]
 
 
 
